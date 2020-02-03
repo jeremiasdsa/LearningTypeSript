7384e0f2b947897b5fc4b5abbec2273f81c92737
jeremiasdsa <jeremias.serafim.araujo@gmail.com>
2020-02-02 23:48:06 -0300

    Learning core types, creating functions on typeScript

--- Course - TypeSript 2020/4 - Config/app.ts
+++ Course - TypeSript 2020/4 - Config/app.ts
@@ -1,3 +1,17 @@
 console.log("Your code goes here...");
 console.log("Serafim code goes here...");
 console.log("jeremias code goes here...");
+
+function add(n1:number, n2: number) {
+    const result = n1 + n2;
+    console.log(result);
+    return n1 + n2;
+}
+const n1 = '5';
+const n2 = 2.8;
+console.log(typeof(n1));
+console.log(typeof(n2));
+
+console.log("Mensagem 2", add(1, 3));
+const resp = add(n1, n2);
+console.log("Mensagem 2", resp);
---
+++ Course - TypeSript 2020/5 - Core Types/Readme.txt
@@ -0,0 +1,16 @@
+Core Types
+
+number
+1
+5.3
+-10
+
+string
+'hi'
+"hi"
+`hi`
+
+boolean
+true 0
+false 1
+
