476cfc1e6d6c3ea20e14d3e8f9e8f3a1f2f5b4b2
jeremiasdsa <jeremias.serafim.araujo@gmail.com>
2020-02-03 13:53:19 -0300

    Learning types

--- Course - TypeSript 2020/4 - Config/app.ts
+++ Course - TypeSript 2020/4 - Config/app.ts
@@ -7,11 +7,24 @@ function add(n1:number, n2: number) {
     console.log(result);
     return n1 + n2;
 }
-const n1 = '5';
+
+function add2(n1:number, n2: number) {
+    if (typeof n1 !== "number" || typeof n2 !== 'number') {
+        throw new Error('Incorrect input');
+    }
+    const result = n1 + n2;
+    console.log(result);
+    return n1 + n2;
+}
+
+const n1 = 5;
 const n2 = 2.8;
+const n3 = '5'
 console.log(typeof(n1));
 console.log(typeof(n2));
 
-console.log("Mensagem 2", add(1, 3));
-const resp = add(n1, n2);
-console.log("Mensagem 2", resp);
+// console.log("Mensagem 2", add(1, 3));
+// const resp = add(n1, n2);
+// console.log("Mensagem 2", resp);
+
+const resp2 = add2(n1, n3);
