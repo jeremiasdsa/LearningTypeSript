593ca00f487239e2fa54c87305ddb138f9d22d33
jeremiasdsa <jeremias.serafim.araujo@gmail.com>
2020-02-08 17:22:21 -0300

    Heroes list, using *nfFor

--- Course - TypeSript 2020/TourOfHeros/angular-tour-of-heros/src/app/heroes/heroes.component.ts
+++ Course - TypeSript 2020/TourOfHeros/angular-tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import  {Hero} from './hero';
+import  {HEROES} from './mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,7 @@ import  {Hero} from './hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "Jeremias",
-    strenght: 5,
-    abilities: ["FIRE", "TIME"]
-  };
+  heroes1 = HEROES;
 
   constructor() { }
 
---
+++ Course - TypeSript 2020/TourOfHeros/angular-tour-of-heros/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,40 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {
+        id: 1,
+        name: "Jeremias",
+        strenght: 5,
+        abilities: ["FIRE", "TIME"]
+      },
+      {
+        id: 2,
+        name: "Marmita",
+        strenght: 5.1,
+        abilities: ["FIRE", "TIME"]
+      },
+      {
+        id: 3,
+        name: "Pareia",
+        strenght: 5.2,
+        abilities: ["FIRE", "TIME"]
+      },
+      {
+        id: 4,
+        name: "Urso",
+        strenght: 5.3,
+        abilities: ["FIRE", "TIME"]
+      },
+      {
+        id: 5,
+        name: "Jordao",
+        strenght: 5.4,
+        abilities: ["FIRE", "TIME"]
+      },
+      {
+        id: 6,
+        name: "Graudo",
+        strenght: 5.5,
+        abilities: ["FIRE", "TIME"]
+      }
+];
